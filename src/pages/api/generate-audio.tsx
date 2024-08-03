9a9d03fc45de141246c3ea2b12cea04396106c42
Lucas Cintra <lucasrcintra63@gmail.com>
2024-08-03 20:28:39 -0300

    ajustando get e trabalhando em criar audios (em andamento)

---
+++ src/pages/api/generate-audio.tsx
@@ -0,0 +1,82 @@
+import { NextApiRequest, NextApiResponse } from "next";
+import fs from "fs";
+import path from "path";
+import fetch from "node-fetch";
+
+const CHUNK_SIZE = 1024;
+
+export default async function handler(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  try {
+    const apiKey = process.env.ELEVENLABS_API_KEY;
+    const voiceId = "FGY2WhTYpPnrIDTdsKH5"; // Substitua com seu voice-id
+    const textToSpeak = "<text>"; // Substitua com o texto desejado
+
+    if (!apiKey) {
+      throw new Error("Chave de API não encontrada");
+    }
+
+    const ttsUrl = `https://api.elevenlabs.io/v1/text-to-speech/${voiceId}/stream`;
+
+    const headers = {
+      Accept: "application/json",
+      "xi-api-key": apiKey,
+      "Content-Type": "application/json",
+    };
+
+    const data = {
+      text: textToSpeak,
+      model_id: "eleven_multilingual_v2",
+      voice_settings: {
+        stability: 0.5,
+        similarity_boost: 0.8,
+        style: 0.0,
+        use_speaker_boost: true,
+      },
+    };
+
+    const response = await fetch(ttsUrl, {
+      method: "POST",
+      headers: headers,
+      body: JSON.stringify(data),
+    });
+
+    if (!response.ok) {
+      const errorText = await response.text();
+      throw new Error(`Falha ao gerar áudio: ${errorText}`);
+    }
+
+    if (!response.body) {
+      throw new Error("Resposta do corpo está vazia.");
+    }
+
+    const outputPath = path.join(process.cwd(), "public", "output.mp3");
+    const fileStream = fs.createWriteStream(outputPath);
+
+    response.body.pipe(fileStream);
+
+    fileStream.on("finish", () => {
+      res
+        .status(200)
+        .json({ message: "Áudio salvo com sucesso", url: "/output.mp3" });
+    });
+
+    fileStream.on("error", (err) => {
+      throw new Error(`Falha ao salvar o áudio: ${err.message}`);
+    });
+
+    // Adicione um listener para o evento 'close' para garantir que a resposta seja enviada se o streaming for interrompido
+    response.body.on("close", () => {
+      if (!res.headersSent) {
+        res.status(500).json({ error: "Falha ao processar o áudio." });
+      }
+    });
+  } catch (error: any) {
+    // Verifique se a resposta não foi enviada antes de enviar um erro
+    if (!res.headersSent) {
+      res.status(500).json({ error: error.message });
+    }
+  }
+}
--- src/pages/api/voices-routes.tsx
+++ src/pages/api/voices-routes.tsx
@@ -12,13 +12,23 @@ export default async function handler(
     }
     console.log("🚀 ~ apiKey:", apiKey);
 
-    const response = await fetch("https://api.elevenlabs.io/v1/voices", {
+    const url = "https://api.elevenlabs.io/v1/voices";
+
+    const response = await fetch(url, {
+      method: "GET",
       headers: {
+        Accept: "application/json",
+        "xi-api-key": apiKey,
         "Content-Type": "application/json",
-        Authorization: `Bearer ${apiKey}`,
       },
     });
 
+    console.log("🚀 ~ response status:", response.status);
+    // Log headers
+    const headers = response.headers;
+    headers.forEach((value, key) => {
+      console.log(`🚀 ~ Header ${key}: ${value}`);
+    });
     if (!response.ok) {
       const erreorText = await response.text();
       throw new Error(`Falha ao listar vozes: ${erreorText}`);
@@ -26,7 +36,7 @@ export default async function handler(
 
     const data = await response.json();
     res.status(200).json(data);
-  } catch (error: any) {
+  } catch (error: Error | any) {
     res.status(500).json({ error: error.message });
   }
 }
